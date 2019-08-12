d444eac024de5cbf2f645d503f30c3ae1f4a7115
Luiz Henrique <luizhenriqque15@gmail.com>
2019-08-12 18:54:34 -0400

    Mobile OK

--- mobile/tindev/src/pages/Login.js
+++ mobile/tindev/src/pages/Login.js
@@ -1,18 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, StyleSheet, Text, TextInput, Image, TouchableOpacity  } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import api from '../services/api';
 import logo from '../assets/logo.png';
  
-export default function Login() {
+export default function Login({ navigation }) {
+    const [user, setUser] = useState('');
+
+    useEffect( () => {
+        AsyncStorage.getItem('user').then(user => {
+            if(user)
+                navigation.navigate('Main',{ user })
+        });
+    }, []);
+
+    async function handlerLogin() {
+        const response = await api.post('devs', {
+            username: user
+        });
+
+        const { _id } = response.data;
+        
+        await AsyncStorage.setItem('user', _id);
+
+        navigation.navigate('Main', { user: _id });
+    }
+
     return(
         <View style={styles.container}>
             <Image source={logo} />
+
             <TextInput
             autoCapitalize="none"
             autoCorrect={false}
             placeholder="Digite seu usuário no Github"
             placeholderTextColor="#999"
-            style={styles.input}/>
-            <TouchableOpacity style={styles.button}>
+            style={styles.input}
+            value={user}
+            onChangeText={setUser}/>
+
+            <TouchableOpacity onPress={handlerLogin} style={styles.button}>
                 <Text style={styles.buttonText}>Enviar</Text>
             </TouchableOpacity>
         </View>
--- mobile/tindev/src/pages/Main.js
+++ mobile/tindev/src/pages/Main.js
@@ -1,8 +1,151 @@
-import React from 'react';
-import { Text} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import { Text, View, SafeAreaView, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import api from '../services/api'
+import logo from '../assets/logo.png';
+import like from '../assets/like.png';
+import dislike from '../assets/dislike.png';
 
-export default function Main(){
+export default function Main({navigation}){
+    const id = navigation.getParam('user');
+    const [users, setUsers] = useState([]);
+    
+    async function handleLogout(){
+        await AsyncStorage.clear();
+        navigation.navigate('Login');
+    }
+
+    async function handleLike(){
+        const [user, ...rest] = users; 
+
+        await api.post(`devs/${user._id}/like`, null, {
+            headers: {user: id}
+        });
+
+        setUsers(rest);
+    }
+    
+    async function handleDeslike(){
+        const [user, ...rest] = users; 
+
+        await api.post(`devs/${user._id}/deslike`, null, {
+            headers: {user:id}
+        });
+
+        setUsers(rest);
+    }
+    
+    useEffect(() => {
+        async function loadUser(){
+            const response = await api.get('/devs',{
+                headers: {user: id}
+            });
+            setUsers(response.data);
+        }
+        loadUser();
+     }, [id])
+
+
     return(
-        <Text>Main</Text>
+        <SafeAreaView style={styles.container}>
+            <TouchableOpacity onPress={handleLogout}>
+                <Image style={styles.logo} source={logo}/>
+            </TouchableOpacity>
+            <View style={styles.cardsContainer}>
+                { users.length === 0 ? 
+                <Text style={styles.empty}> Acabou :(</Text>
+                :
+                (users.map((user, index)=>(
+                    <View key={user._id} style={[styles.card , { zIndex: users.length - index }]}>
+                        <Image style={styles.avatar} source={{uri: user.avatar}} />
+                        <View style={styles.footer }>
+                            <Text style={styles.name }>{ user.name }</Text>
+                            <Text style={styles.bio } numberOfLines={3}>{ user.bio }</Text>
+                        </View>
+                    </View>
+                )))}
+            </View>
+           { users.length > 0 && (
+                <View style={ styles.buttonContainer } >
+                <TouchableOpacity style={styles.button} onPress={handleDeslike}>
+                    <Image source={dislike} />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={handleLike}>
+                    <Image source={like} />
+                </TouchableOpacity>
+            </View>
+           )}
+
+        </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'#f5f5f5',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    cardsContainer:{
+        flex: 1,
+        alignSelf: 'stretch',
+        justifyContent: 'center',
+        maxHeight: 500
+    },
+    card:{
+        borderWidth: 1,
+        borderColor: '#DDD',
+        borderRadius: 8,
+        margin: 30,
+        overflow: 'hidden',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0
+    },
+    logo:{
+        marginTop: 30
+    },
+    empty:{
+        alignSelf: 'center',
+        color: '#999',
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    avatar:{
+        flex: 1,
+        height: 300
+    }, 
+    footer:{
+        backgroundColor: '#fff',
+        paddingHorizontal: 20,
+        paddingVertical: 15
+    },
+    name:{
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#333'
+    },
+    bio:{
+        fontSize: 14,
+        color: '#333',
+        margin: 5,
+        lineHeight: 20
+    },
+    buttonContainer:{
+        flexDirection: 'row',
+        marginBottom: 30
+    },
+    button:{
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        backgroundColor: '#FFF',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginHorizontal: 20,
+        elevation: 2
+    }
+});
